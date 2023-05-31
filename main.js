5137ce11df7d3402248784a1c0d8d85a8dcb5897
mkkim <ilbaleto@naver.com>
2023-06-01 01:41:27 +0900

    refacter logic

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 
 let house = document.getElementById("house");
 
-let _ht = document.querySelector("#h input:nth-of-type(1)");
-let _hx = document.querySelector("#h input:nth-of-type(2)");
-let _r1x = document.querySelector("#r1 input:nth-of-type(1)");
-let _r1y = document.querySelector("#r1 input:nth-of-type(2)");
-let _r2x = document.querySelector("#r2 input:nth-of-type(1)");
-let _r2y = document.querySelector("#r2 input:nth-of-type(2)");
-let _r3x = document.querySelector("#r3 input:nth-of-type(1)");
-let _r3y = document.querySelector("#r3 input:nth-of-type(2)");
-let _r4x = document.querySelector("#r4 input:nth-of-type(1)");
-let _r4y = document.querySelector("#r4 input:nth-of-type(2)");
-let _l1x = document.querySelector("#l1 input:nth-of-type(1)");
-let _l1y = document.querySelector("#l1 input:nth-of-type(2)");
-let _l2x = document.querySelector("#l2 input:nth-of-type(1)");
-let _l2y = document.querySelector("#l2 input:nth-of-type(2)");
-let _l3x = document.querySelector("#l3 input:nth-of-type(1)");
-let _l3y = document.querySelector("#l3 input:nth-of-type(2)");
-let _l4x = document.querySelector("#l4 input:nth-of-type(1)");
-let _l4y = document.querySelector("#l4 input:nth-of-type(2)");
+let _ht = document.getElementById("ht");
+let _hx = document.getElementById("hx");
+let _r1x = document.getElementById("r1x");
+let _r1y = document.getElementById("r1y");
+let _r2x = document.getElementById("r2x");
+let _r2y = document.getElementById("r2y");
+let _r3x = document.getElementById("r3x");
+let _r3y = document.getElementById("r3y");
+let _r4x = document.getElementById("r4x");
+let _r4y = document.getElementById("r4y");
+let _l1x = document.getElementById("l1x");
+let _l1y = document.getElementById("l1y");
+let _l2x = document.getElementById("l2x");
+let _l2y = document.getElementById("l2y");
+let _l3x = document.getElementById("l3x");
+let _l3y = document.getElementById("l3y");
+let _l4x = document.getElementById("l4x");
+let _l4y = document.getElementById("l4y");
 
 let r1c = document.getElementById("r1c");
 let r1s = document.getElementById("r1s");
@@ -54,22 +54,35 @@ function matchRexZero (el) {
  return (re.test(el)) ? '' : el;
 }
 
+function addStorageItem () {
+  if (saved_select.options[saved_select.options.length - 1].value == storage.key(storage.length - 1)) return;
+  let docFrag = document.createDocumentFragment();
+  let option = document.createElement("option");
+  option.textContent = storage.key(storage.length - 1);
+  docFrag.appendChild(option);
+  saved_select.appendChild(docFrag);
+}
 
-window.addEventListener("load", function() {
-
+function addStorageAll () {
   let docFrag = document.createDocumentFragment();
   for (let i = 0; i < storage.length; i++) {
     let option = document.createElement("option");
     option.textContent = storage.key(i);
     docFrag.appendChild(option);
   }
-  saved_select.appendChild(docFrag);
+  saved_select.appendChild(docFrag);  
+}
 
+
+window.addEventListener("load", function() {
+
+  addStorageAll();
 
 });
 
 
 house.addEventListener('change', function(e) {
+
   let ht = valueNum(_ht);
   let hx = valueNum(_hx);
   let r1x = valueNum(_r1x);
@@ -88,7 +101,7 @@ house.addEventListener('change', function(e) {
   let l3y = valueNum(_l3y);
   let l4x = valueNum(_l4x);
   let l4y = valueNum(_l4y);
-  let select = (_select.value == 'sw') ? 53 : (_select.value == 'lw') ? 93 : 106;
+  let select = (_select.value == '53') ? 53 : (_select.value == '93') ? 93 : 106;
   let roll_size = (select == 53) ? 1250 : (select == 93) ? 1775 : 1560;
   let cut_roll = parseInt(roll_size / ht);
 
@@ -158,22 +171,22 @@ house.addEventListener('change', function(e) {
   let l4_side_roll = +(l4_side_cake / cut_roll).toFixed(2);
 
 
-  r1cText = `${r1_ceilPP}평[${r1_ceil_roll}롤]`;
-  r1sText = `${r1_sidePP}평[${r1_side_roll}롤][${r1_side_cake}폭]`;
-  r2cText = `${r2_ceilPP}평[${r2_ceil_roll}롤]`;
-  r2sText = `${r2_sidePP}평[${r2_side_roll}롤][${r2_side_cake}폭]`;
-  r3cText = `${r3_ceilPP}평[${r3_ceil_roll}롤]`;
-  r3sText = `${r3_sidePP}평[${r3_side_roll}롤][${r3_side_cake}폭]`;
-  r4cText = `${r4_ceilPP}평[${r4_ceil_roll}롤]`;
-  r4sText = `${r4_sidePP}평[${r4_side_roll}롤][${r4_side_cake}폭]`;
-  l1cText = `${l1_ceilPP}평[${l1_ceil_roll}롤]`;
-  l1sText = `${l1_sidePP}평[${l1_side_roll}롤][${l1_side_cake}폭]`;
-  l2cText = `${l2_ceilPP}평[${l2_ceil_roll}롤]`;
-  l2sText = `${l2_sidePP}평[${l2_side_roll}롤][${l2_side_cake}폭]`;
-  l3cText = `${l3_ceilPP}평[${l3_ceil_roll}롤]`;
-  l3sText = `${l3_sidePP}평[${l3_side_roll}롤][${l4_side_cake}폭]`;
-  l4cText = `${l4_ceilPP}평[${l4_ceil_roll}롤]`;
-  l4sText = `${l4_sidePP}평[${l4_side_roll}롤][${l4_side_cake}폭]`;
+  let r1cText = `${r1_ceilPP}평[${r1_ceil_roll}롤]`;
+  let r1sText = `${r1_sidePP}평[${r1_side_roll}롤][${r1_side_cake}폭]`;
+  let r2cText = `${r2_ceilPP}평[${r2_ceil_roll}롤]`;
+  let r2sText = `${r2_sidePP}평[${r2_side_roll}롤][${r2_side_cake}폭]`;
+  let r3cText = `${r3_ceilPP}평[${r3_ceil_roll}롤]`;
+  let r3sText = `${r3_sidePP}평[${r3_side_roll}롤][${r3_side_cake}폭]`;
+  let r4cText = `${r4_ceilPP}평[${r4_ceil_roll}롤]`;
+  let r4sText = `${r4_sidePP}평[${r4_side_roll}롤][${r4_side_cake}폭]`;
+  let l1cText = `${l1_ceilPP}평[${l1_ceil_roll}롤]`;
+  let l1sText = `${l1_sidePP}평[${l1_side_roll}롤][${l1_side_cake}폭]`;
+  let l2cText = `${l2_ceilPP}평[${l2_ceil_roll}롤]`;
+  let l2sText = `${l2_sidePP}평[${l2_side_roll}롤][${l2_side_cake}폭]`;
+  let l3cText = `${l3_ceilPP}평[${l3_ceil_roll}롤]`;
+  let l3sText = `${l3_sidePP}평[${l3_side_roll}롤][${l4_side_cake}폭]`;
+  let l4cText = `${l4_ceilPP}평[${l4_ceil_roll}롤]`;
+  let l4sText = `${l4_sidePP}평[${l4_side_roll}롤][${l4_side_cake}폭]`;
 
   r1c.textContent = matchRexZero(r1cText);
   r1s.textContent = matchRexZero(r1sText);
@@ -198,46 +211,82 @@ house.addEventListener('change', function(e) {
 
 saved_select.addEventListener('change', function(e) {
 
-  if (!storage.getItem(saved_select.value)) return;
-
-  let data = JSON.parse(storage.getItem(saved_select.value))
-
-  locKey.value = data.location;
-  h.children[1].value = data.ht;
-  h.children[2].value = data.hx;
-  r1.children[1].value = data.r1x;
-  r1.children[2].value = data.r1y;
-  r2.children[1].value = data.r2x;
-  r2.children[2].value = data.r2y;
-  r3.children[1].value = data.r3x;
-  r3.children[2].value = data.r3y;
-  r4.children[1].value = data.r4x;
-  r4.children[2].value = data.r4y;
-  l1.children[1].value = data.l1x;
-  l1.children[2].value = data.l1y;
-  l2.children[1].value = data.l2x;
-  l2.children[2].value = data.l2y;
-  l3.children[1].value = data.l3x;
-  l3.children[2].value = data.l3y;
-  l4.children[1].value = data.l4x;
-  l4.children[2].value = data.l4y;
-  r1c.textContent = data.r1c;
-  r1s.textContent = data.r1s;
-  r2c.textContent = data.r2c;
-  r2s.textContent = data.r2s;
-  r3c.textContent = data.r3c;
-  r3s.textContent = data.r3s;
-  r4c.textContent = data.r4c;
-  r4s.textContent = data.r4s;
-  l1c.textContent = data.l1c;
-  l1s.textContent = data.l1s;
-  l2c.textContent = data.l2c;
-  l2s.textContent = data.l2s;
-  l3c.textContent = data.l3c;
-  l3s.textContent = data.l3s;
-  l4c.textContent = data.l4c;
-  l4s.textContent = data.l4s;
-  _select.value = data._select;
+  if (storage.getItem(saved_select.value)) {
+    let data = JSON.parse(storage.getItem(saved_select.value));
+    locKey.value = data.location;
+    _ht.value = data.ht;
+    _hx.value = data.hx;
+    _r1x.value = data.r1x;
+    _r1y.value = data.r1y;
+    _r2x.value = data.r2x;
+    _r2y.value = data.r2y;
+    _r3x.value = data.r3x;
+    _r3y.value = data.r3y;
+    _r4x.value = data.r4x;
+    _r4y.value = data.r4y;
+    _l1x.value = data.l1x;
+    _l1y.value = data.l1y;
+    _l2x.value = data.l2x;
+    _l2y.value = data.l2y;
+    _l3x.value = data.l3x;
+    _l3y.value = data.l3y;
+    _l4x.value = data.l4x;
+    _l4y.value = data.l4y;
+    r1c.textContent = data.r1c;
+    r1s.textContent = data.r1s;
+    r2c.textContent = data.r2c;
+    r2s.textContent = data.r2s;
+    r3c.textContent = data.r3c;
+    r3s.textContent = data.r3s;
+    r4c.textContent = data.r4c;
+    r4s.textContent = data.r4s;
+    l1c.textContent = data.l1c;
+    l1s.textContent = data.l1s;
+    l2c.textContent = data.l2c;
+    l2s.textContent = data.l2s;
+    l3c.textContent = data.l3c;
+    l3s.textContent = data.l3s;
+    l4c.textContent = data.l4c;
+    l4s.textContent = data.l4s;
+    _select.value = data._select;   
+  } else {
+    locKey.value = '';
+    _ht.value = '';
+    _hx.value = '';
+    _r1x.value = '';
+    _r1y.value = '';
+    _r2x.value = '';
+    _r2y.value = '';
+    _r3x.value = '';
+    _r3y.value = '';
+    _r4x.value = '';
+    _r4y.value = '';
+    _l1x.value = '';
+    _l1y.value = '';
+    _l2x.value = '';
+    _l2y.value = '';
+    _l3x.value = '';
+    _l3y.value = '';
+    _l4x.value = '';
+    _l4y.value = '';
+    r1c.textContent = '';
+    r1s.textContent = '';
+    r2c.textContent = '';
+    r2s.textContent = '';
+    r3c.textContent = '';
+    r3s.textContent = '';
+    r4c.textContent = '';
+    r4s.textContent = '';
+    l1c.textContent = '';
+    l1s.textContent = '';
+    l2c.textContent = '';
+    l2s.textContent = '';
+    l3c.textContent = '';
+    l3s.textContent = '';
+    l4c.textContent = '';
+    l4s.textContent = '';
+    _select.value = '53'; 
+  }
 
 });
 
@@ -246,24 +295,24 @@ savebtn.onclick = function () {
 
   let myData = {
     location: locKey.value,
-    ht: h.children[1].value,
-    hx: h.children[2].value,
-    r1x: r1.children[1].value,
-    r1y: r1.children[2].value,
-    r2x: r2.children[1].value,
-    r2y: r2.children[2].value,
-    r3x: r3.children[1].value,
-    r3y: r3.children[2].value,
-    r4x: r4.children[1].value,
-    r4y: r4.children[2].value,
-    l1x: l1.children[1].value,
-    l1y: l1.children[2].value,
-    l2x: l2.children[1].value,
-    l2y: l2.children[2].value,
-    l3x: l3.children[1].value,
-    l3y: l3.children[2].value,
-    l4x: l4.children[1].value,
-    l4y: l4.children[2].value,
+    ht: _ht.value,
+    hx: _hx.value,
+    r1x: _r1x.value,
+    r1y: _r1y.value,
+    r2x: _r2x.value,
+    r2y: _r2y.value,
+    r3x: _r3x.value,
+    r3y: _r3y.value,
+    r4x: _r4x.value,
+    r4y: _r4y.value,
+    l1x: _l1x.value,
+    l1y: _l1y.value,
+    l2x: _l2x.value,
+    l2y: _l2y.value,
+    l3x: _l3x.value,
+    l3y: _l3y.value,
+    l4x: _l4x.value,
+    l4y: _l4y.value,
     r1c: r1c.textContent,
     r1s: r1s.textContent,
     r2c: r2c.textContent,
@@ -285,6 +334,8 @@ savebtn.onclick = function () {
 
   if (locKey.value) {
     storage.setItem( locKey.value, JSON.stringify(myData));
+    addStorageItem();
   }
+
 
 }
